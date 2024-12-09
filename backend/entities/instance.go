45199d665b036e023605cf5386fbcb77f01f73ac
snadhiraa2@gm <shofia1912@gmail.com>
2024-12-09 18:59:25 +0700

    Remove backend files from frontend branch

--- backend/entities/instance.go
+++
@@ -1,6 +0,0 @@
-package entities
-
-type Instance struct {
-	Id   int64  `json:"id"`
-	Name string `json:"name"`
-}
--- backend/entities/role.go
+++
@@ -1,6 +0,0 @@
-package entities
-
-type Role struct {
-	Id   int64  `json:"id"`
-	Name string `json:"name"`
-}
